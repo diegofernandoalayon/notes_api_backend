a3871f06cc19938fa1c1e60d8fb86a873f831bd1
Diego Fernando Alayon <dfar.41@gmail.com>
2021-10-19 22:11:20 -0500

    creando esquema con mongoose y guardando primeros datos

--- mongo.js
+++ mongo.js
@@ -6,12 +6,32 @@ const connectionString = 'mongodb+srv://chanchitoFeliz:LACASA.es98@cluster0.d1br
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+  useUnifiedTopology: true
+  // useFindAndModify: false,
+  // useCreateIndex: true
 })
   .then(() => {
     console.log('Database connected')
   }).catch(err => {
     console.error(err)
   })
+
+const noteSchema = new mongoose.Schema({ // esquema para los datos
+  content: String,
+  date: Date,
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema) //
+
+const note = new Note({
+  content: 'Hola mundillo vidilla',
+  date: new Date(),
+  important: false
+})
+note.save()
+  .then(result => {
+    console.log(result)
+    mongoose.connection.close()
+  })
+  .catch(err => console.error(err))
