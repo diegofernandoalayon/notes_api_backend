61d33473bee235d8d7fc5fbd7d833fbc05d67bbd
Diego Fernando Alayon <dfar.41@gmail.com>
2021-10-23 16:21:58 -0500

    escribiendo los primeros test para el back

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const { app, server } = require('../index')
+const Note = require('../models/Note')
 
 const api = supertest(app)
 
@@ -10,10 +11,45 @@ test('notes are returned as json', async () => { // indicamos que es asincrono y
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+const initialNotes = [
+  {
+    content: 'Aprendiendo FullStack bootcamp',
+    important: true,
+    date: new Date()
+  },
+  {
+    content: 'Hola mundo',
+    important: false,
+    date: new Date()
+  },
+  {
+    content: 'Casa de todo',
+    important: false,
+    date: new Date()
+  }
+]
+beforeEach(async () => {
+  await Note.deleteMany({}) // borramos todas las notas
+
+  // initialNotes.forEach(elemen => new Note(elemen).save())
+  const note1 = new Note(initialNotes[0])
+  await note1.save()
+  const note2 = new Note(initialNotes[1])
+  await note2.save()
+  const note3 = new Note(initialNotes[2])
+  await note3.save()
+})
 test('there are two notes', async () => { // indicamos que es asincrono y debe esperar
   const response = await api
     .get('/api/notes')
-  expect(response.body).toHaveLength(11)
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('the first note is about Bootcamp', async () => {
+  const response = await api.get('/api/notes')
+  const contents = response.body.map(note => note.content)
+  expect(contents).toContain('Aprendiendo FullStack bootcamp')
 })
 
 afterAll(() => { // es un hook que se ejecuta despues de todo
