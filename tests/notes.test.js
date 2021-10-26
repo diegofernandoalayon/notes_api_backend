d492d1204be1208875149fcd8923c106fd994edc
Diego Fernando Alayon <dfar.41@gmail.com>
2021-10-26 11:07:36 -0500

    escribiendo test para delete

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -61,6 +61,30 @@ test('note without content is not added', async () => {
   const response = await api.get('/api/notes')
   expect(response.body).toHaveLength(initialNotes.length)
 })
+
+test('a note can be deleted', async () => {
+  const { response: firstResponse } = await getAllContentFromNotes()
+  const { body: notes } = firstResponse
+  const [noteToDelete] = notes
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const { contents, response: secondResponse } = await getAllContentFromNotes()
+  expect(secondResponse.body).toHaveLength(initialNotes.length - 1)
+  expect(contents).not.toContain(noteToDelete.content)
+})
+
+test('a note that do not exist can not be deleted', async () => {
+  await api
+    .delete('/api/notes/1234')
+    .expect(400)
+
+  const { response } = await getAllContentFromNotes()
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
 afterAll(() => { // es un hook que se ejecuta despues de todo
   mongoose.connection.close()
   server.close()
