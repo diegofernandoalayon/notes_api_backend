6c8ef117d23f39b5a691d25fb87de18f7b62101d
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-02 15:54:37 -0500

    agreagando bcrypt para la contraseña, y get para los usuarios

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,22 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/User')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
 
 usersRouter.post('/', async (request, response) => {
   const { body } = request
   const { username, name, password } = body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
   const user = new User({
     username,
     name,
-    passwordHash: password
+    passwordHash
   })
   const savedUser = await user.save()
   response.json(savedUser)
