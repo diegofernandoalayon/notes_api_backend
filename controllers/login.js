d62c5ff0f9c194543366fd4a0f417a2f8e62c9c2
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-03 16:58:53 -0500

    usando jsonwebtoken para crear un token

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/User')
@@ -7,18 +8,25 @@ loginRouter.post('/', async (request, response) => {
   const { username, password } = body
 
   const user = await User.findOne({ username })
+
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
 
-  if (!passwordCorrect) {
+  if (!(user && passwordCorrect)) {
     response.status(401).json({
       error: 'invalid user or password'
-    })
+    }).end()
   }
+  const userForToken = {
+    _id: user._id,
+    username: user.username
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
   response.send({
     name: user.name,
-    username: user.username
+    username: user.username,
+    token
   })
 })
 
