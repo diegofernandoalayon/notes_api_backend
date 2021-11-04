f9298950bb0a9ed4f6ced3cc5f7587aaf7696dbb
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-04 15:47:28 -0500

    creando diccionario para el manejo de errores en el middleware handleErrors

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,9 +1,25 @@
-module.exports = (error, request, response, next) => { // middleware para manejo de errores
-  // console.error(error)
-  // console.log(error.name)
-  if (error.name === 'CastError') {
-    response.status(400).end()
-  } else {
-    response.status(500).end()
-  }
+const ERROR_HANDLERS = {
+  CastError: response =>
+    response.status(400).send({
+      error: 'id used is malformed'
+    }).end(),
+  ValidationError: (response, error) =>
+    response.status(409).send({
+      error: error.menssage
+    }),
+  JsonWebTokenError: response =>
+    response.status(401).json({
+      error: 'token missing or invalid'
+    }),
+  TokenExpirerError: response =>
+    response.status(401).json({
+      error: 'token expired'
+    }),
+  defaultError: response => response.status(500).end()
+}
+
+module.exports = (error, request, response, next) => { //
+  console.error(error.name)
+  const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.defaultError
+  handler(response, error)
 }
